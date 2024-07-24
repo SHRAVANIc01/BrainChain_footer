0f9126badd6bcaeea1a0537dd9367a300df01591
SHRAVANI <shravanichidrewar01@gmail.com>
2024-07-24 18:28:28 +0530

    added

---
+++ footer/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Footer from "./components/Footer";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Footer></Footer>
+    </div>
+  );
+};
+
+export default App;
---
+++ footer/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="f footer">
+      <i className="bi bi-house"></i>
+      <i className="bi bi-people"></i>
+      <i className="bi bi-compass"></i>
+      <i className="bi bi-plus"></i>
+      <i className="bi bi-chat"></i>
+    </div>
+  );
+};
+
+export default Footer;
